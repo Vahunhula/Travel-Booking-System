1b6a1df043eea206b0bf15d3ed1ce14c967aa1a9
Patrick Orjieh <orjiehpatrick@gmail.com>
2023-03-22 22:22:43 +0000

    Implemented testFindCustomerById() - Junit Test and Successful.

--- Travel Booking System/src/Application/JunitTests/MySqlCustomerDaoTest.java
+++ Travel Booking System/src/Application/JunitTests/MySqlCustomerDaoTest.java
@@ -25,4 +25,20 @@ class MySqlCustomerDaoTest {
             e.printStackTrace();
         }
     }
+
+    //this is a test to see if the customer is found by id
+    @org.junit.jupiter.api.Test
+    void testFindCustomerById(){
+        CustomerDaoInterface customerDao = new MySqlCustomerDao();
+        int customerId = 1;
+        try{
+            Customer customer = customerDao.findCustomerById(customerId);
+            //this is a test to see if the customer is found by id
+            //if the customer is found by id, then the customer should not be null
+            assertNotNull(customer);
+            System.out.println(customer);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
