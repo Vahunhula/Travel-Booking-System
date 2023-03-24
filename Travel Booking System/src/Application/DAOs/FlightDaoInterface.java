0fd0f2acc81ec11f1c69de0fe48a05ad85519014
Patrick Orjieh <orjiehpatrick@gmail.com>
2023-03-24 00:14:36 +0000

    Added FlightDaoInterface with Features 1-4.

---
+++ Travel Booking System/src/Application/DAOs/FlightDaoInterface.java
@@ -0,0 +1,23 @@
+package Application.DAOs;
+
+import Application.DTOs.Flight;
+import Application.Exceptions.DaoException;
+
+import java.util.List;
+
+public interface FlightDaoInterface {
+    //Feature 1 – Find all Entities e.g. findAllPlayers() to return a List of all the entities and display
+    //that list
+    public List<Flight> findAllFlights() throws DaoException;
+
+    //Feature 2 – Find and Display (a single) Entity by Key e.g. findPlayerById( id ) – return a single
+    //entity and display its contents.
+    public Flight findFlightById(int flightId) throws DaoException;
+
+    //Feature 3 – Delete an Entity by key e.g. deletePlayerById( id ) – remove entity from database
+    public boolean deleteFlightById(int flightId) throws DaoException;
+
+    //Feature 4 – Insert an Entity in the database using DAO (gather data from user, store in DTO
+    //object, pass into method insertPlayer ( Player ), return new entity including assigned auto-id.
+    public Flight insertFlight(Flight flight) throws DaoException;
+}
