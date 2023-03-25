3b20275df6d565d0d54c31c23f5422433f7f5cec
Patrick Orjieh <orjiehpatrick@gmail.com>
2023-03-25 18:06:31 +0000

    Implemented findBookingById(int bookingId) method

--- Travel Booking System/src/Application/DAOs/MySqlBookingDao.java
+++ Travel Booking System/src/Application/DAOs/MySqlBookingDao.java
@@ -52,5 +52,92 @@ public class MySqlBookingDao extends MySqlDao implements BookingDaoInterface {
         }
         return bookings;
     }
+
+    // @Override
+    //    public Airport findAirportById(int airportId) throws DaoException {
+    //        Connection  connection = null;
+    //        PreparedStatement ps = null;
+    //        ResultSet resultSet = null;
+    //        Airport airport = null;
+    //
+    //        try {
+    //            connection = getConnection();
+    //            String query = "SELECT * FROM airport WHERE airport_id = ?";
+    //            ps = connection.prepareStatement(query);
+    //            ps.setInt(1, airportId);
+    //            resultSet = ps.executeQuery();
+    //
+    //            if (resultSet.next()) {
+    //                String airportName = resultSet.getString("airport_name");
+    //                String airportLocation = resultSet.getString("airport_location");
+    //
+    //                airport = new Airport(airportId,airportName,airportLocation);
+    //            }
+    //        }
+    //        catch (SQLException e) {
+    //            throw new DaoException("findAirportByIdresultSet() " + e.getMessage());
+    //        }
+    //        finally {
+    //            try {
+    //                if (resultSet != null) {
+    //                    resultSet.close();
+    //                }
+    //                if (ps != null) {
+    //                    ps.close();
+    //                }
+    //                if (connection != null) {
+    //                    freeConnection(connection);
+    //                }
+    //            }
+    //            catch (SQLException e) {
+    //                throw new DaoException("findAirportById() " + e.getMessage());
+    //            }
+    //        }
+    //        return airport;
+    //    }
+
+
+    @Override
+    public Booking findBookingById(int bookingId) throws DaoException {
+        Connection connection = null;
+        PreparedStatement ps = null;
+        ResultSet resultSet = null;
+        Booking booking = null;
+
+        try {
+            connection = getConnection();
+            String query = "SELECT * FROM booking WHERE booking_id = ?";
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, bookingId);
+            resultSet = ps.executeQuery();
+
+            if (resultSet.next()) {
+                int customerId = resultSet.getInt("customer_id");
+                int flightId = resultSet.getInt("flight_id");
+                String travelDate = resultSet.getString("travel_date");
+                String travelTime = resultSet.getString("travel_time");
+                int seats = resultSet.getInt("seats");
+
+                booking = new Booking(bookingId,customerId,flightId,travelDate,travelTime,seats);
+            }
+        } catch (SQLException e) {
+            throw new DaoException("findBookingByIdresultSet() " + e.getMessage());
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (ps != null) {
+                    ps.close();
+                }
+                if (connection != null) {
+                    freeConnection(connection);
+                }
+            } catch (SQLException e) {
+                throw new DaoException("findBookingById() " + e.getMessage());
+            }
+        }
+        return booking;
+    }
 }
 
