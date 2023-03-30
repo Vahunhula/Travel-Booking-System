5ae788c1d56e93df5d12620b5164cafd3fa0709d
Patrick Orjieh <orjiehpatrick@gmail.com>
2023-03-30 18:13:57 +0100

    Junit testFindAllPaymentsByBookingNumber() Added

--- Travel Booking System/src/Application/JunitTests/MySqlPaymentDaoTest.java
+++ Travel Booking System/src/Application/JunitTests/MySqlPaymentDaoTest.java
@@ -76,4 +76,24 @@ class MySqlPaymentDaoTest {
             e.printStackTrace();
         }
     }
+
+    //find all payments by booking number
+    @Test
+    void testFindAllPaymentsByBookingNumber(){
+        PaymentDaoInterface paymentDao = new MySqlPaymentDao();
+        String bookingNumber = "B0007";
+        try{
+            List<Payment> payments = paymentDao.findAllPaymentsByBookingNumber(bookingNumber);
+            //this is a test to see if the list is empty
+            assertNotNull(payments);
+            //if the list is not empty, then the size of the list should be greater than 0 which is true
+            assertTrue(payments.size() > 0);
+            for (Payment p : payments) {
+                System.out.println(p);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
