5033645af37aaf1d286d6796f1d01b7e85f1b45b
Patrick Orjieh <orjiehpatrick@gmail.com>
2023-04-04 16:09:18 +0100

    Added DRY to MySqlPaymentDao with helperConn

--- Travel Booking System/src/Application/DAOs/MySqlPaymentDao.java
+++ Travel Booking System/src/Application/DAOs/MySqlPaymentDao.java
@@ -7,62 +7,34 @@ import Application.Exceptions.DaoException;
 import java.sql.*;
 import java.util.*;
 
-public class MySqlPaymentDao extends MySqlDao implements PaymentDaoInterface{
+public class MySqlPaymentDao extends MySqlDao implements PaymentDaoInterface {
     private static TreeSet<String> paymentNumbersCache = new TreeSet<>();
 
-    public void populatePaymentCache() throws DaoException{
-        Connection connection = null;
-        PreparedStatement ps = null;
-        ResultSet resultSet = null;
+    //to get the helper connection
+    private HelperConnection helperConnection = new HelperConnection();
+
+    public void populatePaymentCache() throws DaoException {
         paymentNumbersCache.clear();
-
-        try{
-            connection = getConnection();
-            String query = "SELECT * FROM payment";
-            ps = connection.prepareStatement(query);
-
-            resultSet = ps.executeQuery();
-
-            while(resultSet.next()){
+        try {
+            String query = "SELECT payment_number FROM payment";
+            ResultSet resultSet = helperConnection.executeQuery(query);
+            while (resultSet.next()) {
                 String paymentNumber = resultSet.getString("payment_number");
                 //add the payment number to the cache
                 paymentNumbersCache.add(paymentNumber.toLowerCase());
             }
-
-        }catch(SQLException e){
+        } catch (SQLException e) {
             throw new DaoException("populatePaymentCache() " + e.getMessage());
         }
-        finally{
-            try {
-                if (resultSet != null) {
-                    resultSet.close();
-                }
-                if (ps != null) {
-                    ps.close();
-                }
-                if (connection != null) {
-                    freeConnection(connection);
-                }
-            } catch (SQLException e) {
-                throw new DaoException("populatePaymentCache() " + e.getMessage());
-            }
-        }
     }
+
     @Override
     public List<Payment> findAllPayments() throws DaoException {
-        Connection connection = null;
-        PreparedStatement ps = null;
-        ResultSet resultSet = null;
         List<Payment> payments = new ArrayList<>();
-
-        try{
-            connection = getConnection();
+        try {
             String query = "SELECT * FROM payment";
-            ps = connection.prepareStatement(query);
-
-            resultSet = ps.executeQuery();
-
-            while(resultSet.next()){
+            ResultSet resultSet = helperConnection.executeQuery(query);
+            while (resultSet.next()) {
                 int paymentId = resultSet.getInt("payment_id");
                 String paymentNumber = resultSet.getString("payment_number");
                 String bookingNumber = resultSet.getString("booking_number");
@@ -73,47 +45,23 @@ public class MySqlPaymentDao extends MySqlDao implements PaymentDaoInterface{
                 Payment p = new Payment(paymentId, paymentNumber, bookingNumber, amountPaid, paymentDate, method);
                 payments.add(p);
             }
-        } catch(SQLException e){
+        } catch (SQLException e) {
             throw new DaoException("findAllPayments() " + e.getMessage());
-        } finally{
-            try {
-                if (resultSet != null) {
-                    resultSet.close();
-                }
-                if (ps != null) {
-                    ps.close();
-                }
-                if (connection != null) {
-                    freeConnection(connection);
-                }
-            } catch (SQLException e) {
-                throw new DaoException("findAllPayments() " + e.getMessage());
-            }
         }
         return payments;
     }
 
     @Override
     public Payment findPaymentByNumber(String paymentNumber) throws DaoException {
-        Connection connection = null;
-        PreparedStatement ps = null;
-        ResultSet resultSet = null;
         Payment p = null;
-
         //check if the payment number is in the cache in both upper and lower case
-        if(!paymentNumbersCache.contains(paymentNumber.toLowerCase()) && !paymentNumbersCache.contains(paymentNumber.toUpperCase())){
+        if (!paymentNumbersCache.contains(paymentNumber.toLowerCase()) && !paymentNumbersCache.contains(paymentNumber.toUpperCase())) {
             return null;
         }
-
-        try{
-            connection = getConnection();
+        try {
             String query = "SELECT * FROM payment WHERE LOWER(payment_number) = ?";
-            ps = connection.prepareStatement(query);
-            ps.setString(1, paymentNumber.toLowerCase());
-
-            resultSet = ps.executeQuery();
-
-            if(resultSet.next()){
+            ResultSet resultSet = helperConnection.executeQuery(query, paymentNumber.toLowerCase());
+            if (resultSet.next()) {
                 int paymentId = resultSet.getInt("payment_id");
                 String bookingNumber = resultSet.getString("booking_number");
                 double amountPaid = resultSet.getDouble("amount_paid");
@@ -122,130 +70,54 @@ public class MySqlPaymentDao extends MySqlDao implements PaymentDaoInterface{
 
                 p = new Payment(paymentId, paymentNumber, bookingNumber, amountPaid, paymentDate, method);
             }
-        } catch(SQLException e){
+        } catch (SQLException e) {
             throw new DaoException("findPaymentByNumber() " + e.getMessage());
-        } finally{
-            try {
-                if (resultSet != null) {
-                    resultSet.close();
-                }
-                if (ps != null) {
-                    ps.close();
-                }
-                if (connection != null) {
-                    freeConnection(connection);
-                }
-            } catch (SQLException e) {
-                throw new DaoException("findPaymentByNumber() " + e.getMessage());
-            }
         }
         return p;
     }
 
     @Override
     public boolean deletePaymentByNumber(String paymentNumber) throws DaoException {
-        Connection connection = null;
-        PreparedStatement ps = null;
-        ResultSet resultSet = null;
         boolean deleted = false;
-
-        try{
-            connection = getConnection();
+        try {
             String query = "DELETE FROM payment WHERE LOWER(payment_number) = ?";
-            ps = connection.prepareStatement(query);
-            ps.setString(1, paymentNumber.toLowerCase());
-
-            int result = ps.executeUpdate();
-            if(result == 1){
+            int result = helperConnection.executeUpdate(query, paymentNumber.toLowerCase());
+            if (result == 1) {
                 deleted = true;
-
                 //remove the payment number from the cache
                 paymentNumbersCache.remove(paymentNumber.toLowerCase());
             }
-        } catch(SQLException e){
+        } catch (SQLException e) {
             throw new DaoException("deletePaymentByNumber() " + e.getMessage());
-        } finally{
-            try {
-                if (resultSet != null) {
-                    resultSet.close();
-                }
-                if (ps != null) {
-                    ps.close();
-                }
-                if (connection != null) {
-                    freeConnection(connection);
-                }
-            } catch (SQLException e) {
-                throw new DaoException("deletePaymentByNumber() " + e.getMessage());
-            }
         }
         return deleted;
     }
 
     @Override
     public Payment insertPayment(Payment payment) throws DaoException {
-        Connection connection = null;
-        PreparedStatement ps = null;
-        ResultSet resultSet = null;
         Payment p = null;
-
-        try{
-            connection = getConnection();
+        try {
             String query = "INSERT INTO payment (payment_number, booking_number, amount_paid, payment_date, method) VALUES (?,?,?,?,?)";
-            ps = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-            ps.setString(1, payment.getPayment_number());
-            ps.setString(2, payment.getBooking_number());
-            ps.setDouble(3, payment.getAmount_paid());
-            ps.setString(4, payment.getPayment_date());
-            ps.setString(5, payment.getMethod());
-
-            int result = ps.executeUpdate();
-            if(result == 1){
-                resultSet = ps.getGeneratedKeys();
-                if(resultSet.next()){
-                    int paymentId = resultSet.getInt(1);
-                    p = new Payment(paymentId, payment.getPayment_number().toLowerCase(), payment.getBooking_number(), payment.getAmount_paid(), payment.getPayment_date(), payment.getMethod());
-                }
-
+            int result = helperConnection.executeUpdate(query, payment.getPayment_number(), payment.getBooking_number(), payment.getAmount_paid(), payment.getPayment_date(), payment.getMethod());
+            if (result == 1) {
+                Payment insertedPayment = findPaymentByNumber(payment.getPayment_number());
+                p = insertedPayment;
                 //add the payment number to the cache
                 paymentNumbersCache.add(payment.getPayment_number().toLowerCase());
             }
-        } catch(SQLException e){
+        } catch (SQLException e) {
             throw new DaoException("insertPayment() " + e.getMessage());
-        } finally{
-            try {
-                if (resultSet != null) {
-                    resultSet.close();
-                }
-                if (ps != null) {
-                    ps.close();
-                }
-                if (connection != null) {
-                    freeConnection(connection);
-                }
-            } catch (SQLException e) {
-                throw new DaoException("insertPayment() " + e.getMessage());
-            }
         }
         return p;
     }
 
     @Override
     public List<Payment> findAllPaymentsByBookingNumber(String bookingNumber) throws DaoException {
-        Connection connection = null;
-        PreparedStatement ps = null;
-        ResultSet resultSet = null;
         List<Payment> payments = new ArrayList<>();
-
-        try{
-            connection = getConnection();
+        try {
             String query = "SELECT * FROM payment WHERE LOWER(booking_number) = ?";
-            ps = connection.prepareStatement(query);
-            ps.setString(1, bookingNumber.toLowerCase());
-
-            resultSet = ps.executeQuery();
-
-            while(resultSet.next()){
+            ResultSet resultSet = helperConnection.executeQuery(query, bookingNumber.toLowerCase());
+            while (resultSet.next()) {
                 int paymentId = resultSet.getInt("payment_id");
                 String paymentNumber = resultSet.getString("payment_number");
                 double amountPaid = resultSet.getDouble("amount_paid");
@@ -255,80 +127,35 @@ public class MySqlPaymentDao extends MySqlDao implements PaymentDaoInterface{
                 Payment p = new Payment(paymentId, paymentNumber, bookingNumber, amountPaid, paymentDate, method);
                 payments.add(p);
             }
-        } catch(SQLException e){
+        } catch (SQLException e) {
             throw new DaoException("findAllPaymentsByBookingNumber() " + e.getMessage());
-        } finally{
-            try {
-                if (resultSet != null) {
-                    resultSet.close();
-                }
-                if (ps != null) {
-                    ps.close();
-                }
-                if (connection != null) {
-                    freeConnection(connection);
-                }
-            } catch (SQLException e) {
-                throw new DaoException("findAllPaymentsByBookingNumber() " + e.getMessage());
-            }
         }
         return payments;
     }
 
     @Override
     public Set<String> uniquePaymentMethod() throws DaoException {
-        Connection connection = null;
-        PreparedStatement ps = null;
-        ResultSet resultSet = null;
         Set<String> paymentMethods = new HashSet<>();
-
-        try{
-            connection = getConnection();
+        try {
             String query = "SELECT DISTINCT method FROM payment";
-            ps = connection.prepareStatement(query);
-
-            resultSet = ps.executeQuery();
-
-            while(resultSet.next()){
+            ResultSet resultSet = helperConnection.executeQuery(query);
+            while (resultSet.next()) {
                 String method = resultSet.getString("method");
                 paymentMethods.add(method);
             }
-        } catch(SQLException e){
+        } catch (SQLException e) {
             throw new DaoException("uniquePaymentMethod() " + e.getMessage());
-        } finally{
-            try {
-                if (resultSet != null) {
-                    resultSet.close();
-                }
-                if (ps != null) {
-                    ps.close();
-                }
-                if (connection != null) {
-                    freeConnection(connection);
-                }
-            } catch (SQLException e) {
-                throw new DaoException("uniquePaymentMethod() " + e.getMessage());
-            }
         }
         return paymentMethods;
     }
 
     @Override
     public List<Payment> findPaymentByPaymentMethod(String paymentMethod) throws DaoException {
-        Connection connection = null;
-        PreparedStatement ps = null;
-        ResultSet resultSet = null;
         List<Payment> payments = new ArrayList<>();
-
-        try{
-            connection = getConnection();
+        try {
             String query = "SELECT * FROM payment WHERE LOWER(method) = ?";
-            ps = connection.prepareStatement(query);
-            ps.setString(1, paymentMethod.toLowerCase());
-
-            resultSet = ps.executeQuery();
-
-            while(resultSet.next()){
+            ResultSet resultSet = helperConnection.executeQuery(query, paymentMethod.toLowerCase());
+            while (resultSet.next()) {
                 int paymentId = resultSet.getInt("payment_id");
                 String paymentNumber = resultSet.getString("payment_number");
                 String bookingNumber = resultSet.getString("booking_number");
@@ -338,22 +165,8 @@ public class MySqlPaymentDao extends MySqlDao implements PaymentDaoInterface{
                 Payment p = new Payment(paymentId, paymentNumber, bookingNumber, amountPaid, paymentDate, paymentMethod);
                 payments.add(p);
             }
-        } catch(SQLException e){
+        } catch (SQLException e) {
             throw new DaoException("findPaymentByPaymentMethod() " + e.getMessage());
-        } finally{
-            try {
-                if (resultSet != null) {
-                    resultSet.close();
-                }
-                if (ps != null) {
-                    ps.close();
-                }
-                if (connection != null) {
-                    freeConnection(connection);
-                }
-            } catch (SQLException e) {
-                throw new DaoException("findPaymentByPaymentMethod() " + e.getMessage());
-            }
         }
         return payments;
     }
